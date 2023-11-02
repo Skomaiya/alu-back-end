003b4a708ec1b5763c93b086f41446f3cc1f2e05
Komaiya Samuel <s.komaiya@alustudent.com>
2023-11-02 02:20:42 -0700

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,35 +1,43 @@
 #!/usr/bin/python3
+
 import csv
 import json
 import requests
 from sys import argv
 
+# Define HTTP headers for the API requests
 headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
 }
+
+# Define the header for the CSV file
 csvheader = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
 if __name__ == "__main__":
-    """ Functions for gathering  data from an API """
+    # Step 1: Retrieve user information from the JSONPlaceholder API using the provided user ID.
+    user_id = argv[1]  # Get the user ID from the command-line argument
     request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+        f'https://jsonplaceholder.typicode.com/users/{user_id}')
     employee = json.loads(request_employee.text)
     employee_name = employee.get("name")
     userName = employee.get("username")
+
+    # Step 2: Retrieve the user's tasks from the API.
     request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+        f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
     tasks = []
     employee_todos = json.loads(request_todos.text)
-    
-    USER_ID = argv[1]
-    
+
+    # Step 3: Create a list of tasks including user ID, username, task completion status, and task title.
     for dictionary in employee_todos:
         tasks.append([dictionary.get("userId"), userName, dictionary.get("completed"), dictionary.get("title")])
-          
-with open(f'{USER_ID}.csv', 'w', encoding="UTF8", newline='') as user:
-    writer = csv.writer(user)
 
-    writer.writerow(csvheader)
-    writer.writerows(tasks)
+    # Step 4: Generate the CSV filename based on the user's ID.
+    USER_ID = user_id
 
+    # Step 5: Create and write the data to a CSV file with the generated filename.
+    with open(f'{USER_ID}.csv', 'w', encoding="UTF8", newline='') as user:
+        writer = csv.writer(user)
+        writer.writerow(csvheader)
+        writer.writerows(tasks)
