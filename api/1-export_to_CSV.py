f36d570670ba83ac70ee0cec6fc46f175769c484
Komaiya Samuel <s.komaiya@alustudent.com>
2023-11-02 02:29:47 -0700

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
-
 import csv
 import json
 import requests
 from sys import argv
 
-# Define HTTP headers for the API requests
+""" Define HTTP headers for the API requests"""
 headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
 }
 
-# Define the header for the CSV file
+"""Define the header for the CSV file"""
 csvheader = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
 if __name__ == "__main__":
-    # Step 1: Retrieve user information from the JSONPlaceholder API using the provided user ID.
-    user_id = argv[1]  # Get the user ID from the command-line argument
+    """Get the user ID from the command-line argument"""
+    user_id = argv[1]
+    """ Step 1: Retrieve user information from the JSONPlaceholder API using the provided user ID."""
     request_employee = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{user_id}')
     employee = json.loads(request_employee.text)
     employee_name = employee.get("name")
     userName = employee.get("username")
 
-    # Step 2: Retrieve the user's tasks from the API.
+    """ Step 2: Retrieve the user's tasks from the API."""
     request_todos = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
     tasks = []
     employee_todos = json.loads(request_todos.text)
 
-    # Step 3: Create a list of tasks including user ID, username, task completion status, and task title.
+    """ Step 3: Create a list of tasks including user ID, username, task completion status, and task title."""
     for dictionary in employee_todos:
         tasks.append([dictionary.get("userId"), userName, dictionary.get("completed"), dictionary.get("title")])
 
-    # Step 4: Generate the CSV filename based on the user's ID.
+    """ Step 4: Generate the CSV filename based on the user's ID."""
     USER_ID = user_id
 
-    # Step 5: Create and write the data to a CSV file with the generated filename.
+    """Step 5: Create and write the data to a CSV file with the generated filename."""
     with open(f'{USER_ID}.csv', 'w', encoding="UTF8", newline='') as user:
         writer = csv.writer(user)
         writer.writerow(csvheader)
         writer.writerows(tasks)
+
